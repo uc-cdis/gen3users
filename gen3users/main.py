d6c3ae19c9a830e9854944a0aed4c50ade3c2e29
Pauline Ribeyre <ribeyre@uchicago.edu>
2019-05-17 16:30:17 -0500

    feat(cli): check subcommand name

--- gen3users/main.py
+++ gen3users/main.py
@@ -8,7 +8,7 @@ def get_command_line_args():
     parser = argparse.ArgumentParser(
         description="Utils for Gen3 commons user management"
     )
-    subs = parser.add_subparsers()
+    subs = parser.add_subparsers(dest="command")
     validate = subs.add_parser("validate", help="Validate one or more user.yaml files")
     validate.add_argument(
         "file_names",
@@ -24,20 +24,24 @@ def get_command_line_args():
 def main(args=None):
     if args is None:
         args = get_command_line_args()
-    
+
     # validate user.yaml files
-    failed_validation = False
-    for file_name in getattr(args, "file_names", []):
-        try:
-            with open(file_name, "r") as f:
-                user_yaml = f.read()
-                validate_user_yaml(user_yaml, file_name)
-        except Exception as e:
-            print(e)
-            failed_validation = True
-        print('')
-    if failed_validation:
-        raise Exception("user.yaml validation failed. See errors in previous logs.")
+    if args.command == "validate":
+        failed_validation = False
+        for file_name in getattr(args, "file_names", []):
+            try:
+                with open(file_name, "r") as f:
+                    user_yaml = f.read()
+                    validate_user_yaml(user_yaml, file_name)
+            except Exception as e:
+                print(e)
+                failed_validation = True
+            print("")
+        if failed_validation:
+            raise Exception("user.yaml validation failed. See errors in previous logs.")
+
+    else:
+        raise Exception("Unknown subcommand {}".format(args.command))
 
 
 if __name__ == "__main__":
